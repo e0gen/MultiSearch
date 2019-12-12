cd20d06a3ff46007c6a857978f5250b8487a75f5
Evgeny <enevzorov@yandex.ru>
2019-12-12 17:36:04 +0300

    Domain & Search engines

---
+++ MultiSearch.Engines/BingSerach/BingCustomSearchResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchEngines
+{
+    public class BingCustomSearchResponse
+    {
+        public string _type { get; set; }
+        public WebPages webPages { get; set; }
+    }
+
+    public class WebPages
+    {
+        public string webSearchUrl { get; set; }
+        public int totalEstimatedMatches { get; set; }
+        public WebPage[] value { get; set; }
+    }
+
+    public class WebPage
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string displayUrl { get; set; }
+        public string snippet { get; set; }
+        public DateTime dateLastCrawled { get; set; }
+        public string cachedPageUrl { get; set; }
+        public OpenGraphImage openGraphImage { get; set; }
+    }
+
+    public class OpenGraphImage
+    {
+        public string contentUrl { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}
---
+++ MultiSearch.Engines/GoogleSearch/GoogleSearch.cs
@@ -0,0 +1,43 @@
+﻿using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+using MultiSearch.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace SearchEngines
+{
+    public class GoogleSearch : ISearch
+    {
+        private readonly string _apiKey; // = "AIzaSyAt8AkrmkiLVghrcKA3lFh37R79rSG0NsE";
+        private readonly string _searchEngineId; // = "003470263288780838160:ty47piyybua";
+        private readonly CustomsearchService _customSearchService;// );
+
+        public GoogleSearch(string apiKey, string searchEngineId)
+        {
+            _apiKey = apiKey;
+            _searchEngineId = searchEngineId;
+            _customSearchService = new CustomsearchService(new BaseClientService.Initializer { ApiKey = apiKey });
+        }
+
+        public IEnumerable<Item> Search(string query)
+        {
+            var listRequest = _customSearchService.Cse.List(query);
+            listRequest.Cx = _searchEngineId;
+            
+            IList<Result> paging = new List<Result>();
+            var count = 0;
+            while (paging != null)
+            {
+                listRequest.Start = count * 10 + 1;
+                paging = listRequest.Execute().Items;
+                if (paging != null)
+                    foreach (var item in paging)
+                    {
+                        yield return new Item(item.Title, item.Link, item.Snippet, "Google");
+                    }
+                count++;
+            }
+        }
+    }
+}
